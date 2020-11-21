2b239ca8c96160e37698889c8bb09abcd024907d
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2020-11-21 12:48:24 +0100

    Basic - fix nazw.

---
+++ basic_pgg/zad_3.py
@@ -0,0 +1,11 @@
+"""
+Napisz program wyliczający kwotę należną za zakupiony
+towar na podstawie ceny za kilogram oraz liczby zakupionych kilogramów.
+Do przechowywania informacji o cenie oraz liczbie kilogramów użyj zmiennych.
+Wypisz wszystkie informacje na konsolę.
+
+Przykładowy komunikat programu:
+Cena za kg: 10.0
+Waga: 2.5
+Należność: 25.0
+"""
