f090cfcaa9c0ab652e72b73360a2ec85b1515b88
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2020-11-22 09:34:32 +0100

    Basic - zad 11.

--- basic_pgg/zad_11.py
+++ basic_pgg/zad_11.py
@@ -8,8 +8,10 @@ suma_temperatur = 0
 LICZBA_DNI = 7
 
 while numer_dnia <= LICZBA_DNI:
-    suma_temperatur += int(input("Podaj temperature: "))
+    suma_temperatur += int(input(f"Podaj temperature z dnia {numer_dnia}: "))
     numer_dnia += 1
+
+# LICZBA_DNI = 1
 
 srednia_temperatur = suma_temperatur / LICZBA_DNI
 
