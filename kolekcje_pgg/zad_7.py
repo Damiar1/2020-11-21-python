6fd314135c462a472b7bb08b1710ef690ce8ee8e
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2020-11-22 16:59:53 +0100

    Kolekcje - zad 7.

--- kolekcje_pgg/zad_7.py
+++ kolekcje_pgg/zad_7.py
@@ -5,7 +5,6 @@ Do przechowywania informacji o cenie za kilogram
 danego produktu użyj słownika.
 Wypisz wszystkie dostępne produkty w sklepie.
 
-
 1. Robimy słownik z produktami, dodajmy:
       ziemniaki 1.2
       pomiodory 4.5
@@ -15,3 +14,24 @@ Wypisz wszystkie dostępne produkty w sklepie.
 4. Wczytujemy ile kg uzytkownik chce kupic
 5. Liczymy należnosc
 """
+
+produkty = {
+    'ziemniaki': 1.2,
+    'pomidory': 4.5,
+    'marchewki': 0.5,
+}
+
+for produkt, cena in produkty.items():
+    print(f'{produkt} - {cena:.2f} zł / kg')
+
+co = input('Co chcesz kupić z tej listy? ')
+
+if co not in produkty:
+    print("Nie ma takiego produktu!")
+    exit()
+
+liczba_kg = float(input(f'Ile kg produktu {co} chcesz kupic?'))
+
+kwota = produkty[co] * liczba_kg
+
+print(f'Za {liczba_kg} kg produktu {co} zapłacisz {kwota:.2f} zł.')
