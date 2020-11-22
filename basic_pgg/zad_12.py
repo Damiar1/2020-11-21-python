187290098bcc3eaf644b6b1f116871f911840272
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2020-11-22 10:33:56 +0100

    Basic - zad 12.

--- basic_pgg/zad_12.py
+++ basic_pgg/zad_12.py
@@ -10,8 +10,9 @@ znalezione_minimum = None
 znalezione_maksimum = None
 
 while True:
+    # funkcja input ZAWSZE zwraca string!
     dane_wejsciowe = input("Podaj liczbe lub wpisz KONIEC: ")
-    if dane_wejsciowe == "KONIEC":
+    if dane_wejsciowe.upper() == "KONIEC":
         break
 
     liczba = int(dane_wejsciowe)
@@ -21,7 +22,8 @@ while True:
     if znalezione_maksimum is None or liczba > znalezione_maksimum:
         znalezione_maksimum = liczba
 
-print(f'Znalezione minimum: {znalezione_minimum}')
-print(f'Znalezione maksimum: {znalezione_maksimum}')
-
-
+if znalezione_minimum is not None and znalezione_maksimum is not None:
+    print(f'Znalezione minimum: {znalezione_minimum}')
+    print(f'Znalezione maksimum: {znalezione_maksimum}')
+else:
+    print('Nie podales liczb!')
