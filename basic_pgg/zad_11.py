d0b3e43d61de79e68a6b3fb23f3d3edaee12caa2
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2020-11-22 09:28:26 +0100

    Basic - zad 11.

---
+++ basic_pgg/zad_11.py
@@ -0,0 +1,16 @@
+"""
+Napisz program obliczający średnią wartość temperatury w danym tygodniu
+na podstawie temperatur wprowadzonych przez użytkownika.
+"""
+
+numer_dnia = 1
+suma_temperatur = 0
+LICZBA_DNI = 7
+
+while numer_dnia <= LICZBA_DNI:
+    suma_temperatur += int(input("Podaj temperature: "))
+    numer_dnia += 1
+
+srednia_temperatur = suma_temperatur / LICZBA_DNI
+
+print(f'Srednia temperatura w tygodniu to {srednia_temperatur:.1f}')
