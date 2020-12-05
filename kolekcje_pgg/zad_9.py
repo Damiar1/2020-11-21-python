000f89c0a6fdb5afab7e6c60d5d66b8ad24598f6
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-05 11:38:36 +0100

    zadanie 9 - rozwiązanie

--- kolekcje_pgg/zad_9.py
+++ kolekcje_pgg/zad_9.py
@@ -7,6 +7,8 @@ b ma mieć zawartość: [400, 90, 100, 234 ... ] itd
 a = [1, 400, 3, 7, 20, 90, 100, 234, 3423, 40, 4343, 1, 1000 ]
 b = []
 
-# TODO
+for x in a:
+    if x > 50:
+        b.append( x )
 
 print(b)
