3ccbfcec9a4b09e2eaf53dc84d31f12ec5d828fc
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-05 16:44:02 +0100

    zadanie 1 z funkcji

---
+++ funkcje/zadanie1.py
@@ -0,0 +1,25 @@
+"""
+Napisać kod funkcji pokaz_ucznia, która wyświetli informacje o uczniu w takiej formie:
+
+Imie:       Jan
+Nazwisko:   Kowalski
+Oceny:      3,5,6,3,2...
+Średnia:    4.59
+
+funkcja ma być wywoływana w taki sposób:
+
+pokaz_ucznia( uczen1 )
+
+"""
+
+uczen1 = {"imie": "Katarzyna", "nazwisko": "H.", "oceny": "3 4 4 5 4 3 3 5 5 5"}
+uczen2 = { "imie" : "Kosma", "nazwisko" : "I.", "oceny" : "3 4 4 3 3 3 4 4 4 4" }
+
+def pokaz_ucznia( uczen ):
+    print("Imie: ...")
+    # TODO
+
+# wywołanie funkcji:
+pokaz_ucznia(uczen1)
+#pokaz_ucznia(uczen2)
+
