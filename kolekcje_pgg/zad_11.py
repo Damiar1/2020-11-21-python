b604b50529808b5a5c0f9f6cbdb148b473e593fd
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-05 13:53:04 +0100

    zadanie 11

---
+++ kolekcje_pgg/zad_11.py
@@ -0,0 +1,17 @@
+"""
+Napisać program, który zamieni numer dnia (0-6) na dwuliterowy symbol dnia tygodnia
+
+0 - Ni
+1 - Po
+2 - Wt
+3 - Śr
+4 - Cz
+5 - Pi
+6 - So
+
+"""
+n = int(input("Podaj numer dnia tygodnia (0-6): "))
+
+# TODO: (nie chodzi o if!!!)
+
+print(f"Kod dnia: {kod}")
