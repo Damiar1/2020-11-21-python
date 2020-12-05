6b5b45d754cba69b1ba1e25dd0a4b24183c731f0
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-05 12:06:10 +0100

    zadanie 9 bis - zapis skrócony

--- kolekcje_pgg/zad_9_bis.py
+++ kolekcje_pgg/zad_9_bis.py
@@ -5,10 +5,5 @@ b ma mieć zawartość: [400, 90, 100, 234 ... ] itd
 """
 
 a = [1, 400, 3, 7, 20, 90, 100, 234, 3423, 40, 4343, 1, 1000 ]
-b = []
-
-for x in a:
-    if x > 50:
-        b.append( x )
-
+b = [ x for x in a if x > 50 ]
 print(b)
