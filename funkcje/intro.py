cd7c03f14464ffb66605cf9ddd10212d01709ef3
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-05 16:32:37 +0100

    przykładowe funkcje

---
+++ funkcje/intro.py
@@ -0,0 +1,23 @@
+# Czy znamy jakieś funkcje?
+# TAK!
+
+# Najprostsza funkcja
+def powitanie():
+    print("Dzien dobry")
+    print("...")
+    print("Jaki piękny dzień")
+
+powitanie()
+
+print("--" * 20)
+
+def powitanie2(imie):
+    x = 7
+    print(f"Witaj, {imie}")
+    print(f"Czyż nie jest piękny {pora_dnia}?")
+    for i in range(3):
+        print("Co nie?")
+
+pora_dnia = "noc"
+powitanie2("Mateusz")
+
