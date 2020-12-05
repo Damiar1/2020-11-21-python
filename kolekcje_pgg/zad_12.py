9d9a84166e18638a472b8f395e2aed8f2e573e59
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-05 15:16:24 +0100

    zadanie 12 - rozwiązanie

--- kolekcje_pgg/zad_12.py
+++ kolekcje_pgg/zad_12.py
@@ -3,3 +3,17 @@ Napisz program zliczajacy liczbe unikalnych liczb wprowadzonych
 przez uzytkownika. Sprawdz jak duzo z tych liczb jest liczbami
 parzystymi w zakresie 0-100.
 """
+
+liczby = set()
+
+while True:
+    x = input("Podaj liczbe (lub KONIEC):")
+    if x.lower() == 'koniec':
+        break
+    xx = int(x)
+    if 0 <= xx <= 100 and xx % 2 == 0:
+        liczby.add(xx)
+
+print(f"Zbiór: {liczby}")
+print(f"Liczba elementów: {len(liczby)}")
+
