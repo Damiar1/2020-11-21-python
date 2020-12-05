4abf5e21870275cdfcd5dcfc6577f90b9c43f607
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-05 11:25:08 +0100

    zadanie 8

---
+++ kolekcje_pgg/zad_8.py
@@ -0,0 +1,7 @@
+"""
+Mam listę liczb w zmiennej a.
+chcemy w zmiennej b stworzyć listę zawierającą pięciokrotności każdej z liczb, czyli np.
+b ma mieć zawartość: [5, 15, 35, 100 ... ] itd
+"""
+
+a = [1, 3, 7, 20, 90, 100, 234, 3423, 4343]
