9b158f984dd3c0332da07fbc36f732c4c6138239
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 12:49:04 +0100

    zadanie - temperatury

--- funkcje/zadanie5.py
+++ funkcje/zadanie5.py
@@ -10,3 +10,27 @@ Napisać 6 funkcji przeliczających temperatury:
 
 # def przelicz_C_na_K(t):
 # itd
+
+def przelicz_C_na_K(t):
+    return t + 273.15
+
+def przelicz_K_na_C(t):
+    return t - 273.15
+
+def przelicz_C_na_F(t):
+    return 1.8 * t + 32
+
+def przelicz_F_na_C(t):
+    return (t - 32)/1.8
+
+def przelicz_K_na_F(t):
+    return przelicz_C_na_F(przelicz_K_na_C(t))
+
+def przelicz_F_na_K(t):
+    return przelicz_C_na_K(przelicz_F_na_C(t))
+
+
+temperatury = [ -50, -20, -10, -1, 0, 1, 5, 15, 25, 70, 100, 200, 300 ]
+
+for temperatura in temperatury:
+    print(f"{temperatura:10.2f}C = {przelicz_C_na_F(temperatura):10.2f}F = {przelicz_C_na_K(temperatura):10.2f}K")
