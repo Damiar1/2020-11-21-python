e5bedc21f6fe951f86f1b9be03daf664e1e0b6e5
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 15:45:58 +0100

    zadanie 7 - rozwiązanie

--- funkcje/zadanie7.py
+++ funkcje/zadanie7.py
@@ -4,11 +4,17 @@ która zamieni listę z zagnieżdżonymi listami, na zwykłą listę liczb.
 
 """
 
-x = [ 5, 6, 76, 45, [ 234, 43, 34, [ 43, 43 ], [34, 43], [43] ], 100 ]
+x = [ 5, 6, 76, 45, [ 234, 43, 34, [ 43, [[43]] ], [34, [[43]]], [43] ], 100 ]
 
 # TODO
-# def splaszcz(lista):
-#
+def splaszcz(lista):
+    wynik = []
+    for e in lista:
+        if type(e) == int:
+            wynik += [ e ]
+        if type(e) == list:
+            wynik += splaszcz(e)
+    return wynik
 
 x2 = splaszcz( x )
 print(x2)
