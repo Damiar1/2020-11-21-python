4cb25797efba5a6d565727e3684ea3dc4372ff33
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 09:31:27 +0100

    rozwiązanie zadania pokaz_ucznia

--- funkcje/zadanie1.py
+++ funkcje/zadanie1.py
@@ -9,15 +9,18 @@ Oceny:      3,5,6,3,2...
 funkcja ma być wywoływana w taki sposób:
 
 pokaz_ucznia( uczen1 )
-
 """
 
 uczen1 = {"imie": "Katarzyna", "nazwisko": "H.", "oceny": "3 4 4 5 4 3 3 5 5 5"}
 uczen2 = { "imie" : "Kosma", "nazwisko" : "I.", "oceny" : "3 4 4 3 3 3 4 4 4 4" }
 
 def pokaz_ucznia( uczen ):
-    print("Imie: ...")
-    # TODO
+    oceny = [int(o) for o in uczen['oceny'].split()]
+    srednia = sum(oceny)/len(oceny)
+    print(f"{'Imie:':11} {uczen['imie']}")
+    print(f"{'Nazwisko:':11} {uczen['nazwisko']}")
+    print(f"{'Oceny:':11} {','.join([str(o) for o in oceny])}")
+    print(f"{'Średnia':11} {srednia}")
 
 # wywołanie funkcji:
 pokaz_ucznia(uczen1)
