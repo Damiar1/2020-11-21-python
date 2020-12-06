6ada3a8f123e1d5eab9b5fc55d944050c973247c
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 15:48:08 +0100

    obsługa innych typów, spłaszczanie tylko list

--- funkcje/zadanie7.py
+++ funkcje/zadanie7.py
@@ -6,14 +6,13 @@ która zamieni listę z zagnieżdżonymi listami, na zwykłą listę liczb.
 
 x = [ 5, 6, 76, 45, [ 234, 43, 34, [ 43, [[43]] ], [34, [[43]]], [43] ], 100 ]
 
-# TODO
 def splaszcz(lista):
     wynik = []
     for e in lista:
-        if type(e) == int:
-            wynik += [ e ]
         if type(e) == list:
             wynik += splaszcz(e)
+        else:
+            wynik += [ e ]
     return wynik
 
 x2 = splaszcz( x )
