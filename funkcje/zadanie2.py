f4800c004e5e60c6844862151649eca7aaa32de0
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 11:10:13 +0100

    korekta ocen

--- funkcje/zadanie2.py
+++ funkcje/zadanie2.py
@@ -1,14 +1,14 @@
 uczniowie = [
   { "imie" : "Julian", "nazwisko" : "P.", "oceny" : "3 6 5 3 5 3 2 3 4 3 3" },
-  { "imie" : "Agnieszka", "nazwisko" : "I.", "oceny" : "2 4 4 3 5 2 2 5 3 4 2" },
+  { "imie" : "Agnieszka", "nazwisko" : "I.", "oceny" : "6 4 4 6 5 6 2 6 6 6 2" },
   { "imie" : "Magdalena", "nazwisko" : "M.", "oceny" : "4 4 3 3 3 4 4 3 4 4 3" },
   { "imie" : "Jacek", "nazwisko" : "L.", "oceny" : "5 3 3 4 4 5 2 4 3 4 3" },
   { "imie" : "Marta", "nazwisko" : "S.", "oceny" : "5 5 5 2 2 5 5 2 3 2 5" },
   { "imie" : "Jacek", "nazwisko" : "Z.", "oceny" : "2 5 5 3 5 3 3 3 5 4 3" },
-  { "imie" : "Mateusz", "nazwisko" : "N.", "oceny" : "6 5 3 4 5 5 2 4 3 3 2" },
+  { "imie" : "Mateusz", "nazwisko" : "N.", "oceny" : "6 5 6 5 5 5 6 4 5 5 6" },
   { "imie" : "Karol", "nazwisko" : "I.", "oceny" : "3 4 3 3 3 3 3 4 3 4 4" },
   { "imie" : "Miriam", "nazwisko" : "L.", "oceny" : "4 3 2 4 3 4 3 3 3 4 4" },
-  { "imie" : "Julian", "nazwisko" : "Y.", "oceny" : "4 4 4 4 3 4 3 4 3 3 4" },
+  { "imie" : "Julian", "nazwisko" : "Y.", "oceny" : "4 6 4 5 6 4 6 5 6 6 4" },
   { "imie" : "Marta", "nazwisko" : "S.", "oceny" : "4 3 3 4 4 3 4 3 4 3 4" },
   { "imie" : "Mateusz", "nazwisko" : "N.", "oceny" : "3 3 5 6 6 5 4 6 4 3 3" },
   { "imie" : "Jacek", "nazwisko" : "J.", "oceny" : "3 5 2 4 4 1 3 5 4 2 3" },
@@ -27,6 +27,10 @@ def imie_i_nazwisko_ucznia(uczen):
 
 def podsumowanie(uczen):
   return imie_i_nazwisko_ucznia(uczen), srednia_ucznia(uczen)
+
+#TODO: napisac funkcję czy_czerwony_pasek(uczen)
+# ktora zwraca True albo False
+# w zależności, czy uczeń zasługuje na czerwony pasek (śred >= 4.75)
 
 for u in uczniowie:
     imie_n, sr = podsumowanie(u)
