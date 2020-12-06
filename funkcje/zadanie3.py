5a3c48799d4c683176433067488d31f65eaa2e67
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 11:35:06 +0100

    pozbycie się zmiennej tymczasowej

--- funkcje/zadanie3.py
+++ funkcje/zadanie3.py
@@ -7,8 +7,7 @@ przy zakupie co najmniej 10 biletów bilet kosztuje 13
 # TODO: zaimplementować funkcję koszt_biletow_grupy(liczba_osob)
 
 def koszt_biletow_grupy(liczba_osob):
-    cena_biletu = 13 if liczba_osob >= 10 else 20
-    return liczba_osob * cena_biletu
+    return liczba_osob * 13 if liczba_osob >= 10 else 20
 
 grupy_testowe = [ 1, 2, 5, 7, 9, 13, 20, 50 ]
 
