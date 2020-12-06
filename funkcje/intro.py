6492287e3a2acbcc212188466b1ab4962e721aab
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 10:30:59 +0100

    wielokrotny return

--- funkcje/intro.py
+++ funkcje/intro.py
@@ -56,3 +56,25 @@ print(b)
 miesiace = "styczen luty marzec "
 c = dwukrotnosc(miesiace)
 print(c)
+
+
+def znak_liczby(a):
+    print(f"Sprawdzam znak liczby {a}")
+    print(f"- sprawdzam czy mniejsza od zera...")
+    if a < 0:
+        print("Tak! Mniejsza od zera!")
+        return -1
+    print(f"- sprawdzam czy większa od zera...")
+    if a > 0:
+        print("Tak! Większa od zera!")
+        return 1
+    print("Sprawdzam czy może zero???")
+    if a == 0:
+        print("Tak! To zero!")
+        return 0
+    print("To się właściwie nie powinno wydarzyć...")
+
+liczby = [1,3,5,0,-4,4,-99]
+
+for n in liczby:
+    print(f"{n} => {znak_liczby(n)}")
