b05602e3e71e695630c644a9fe195ec13b34d5f7
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 12:56:44 +0100

    zadanie - temperatury c.d.

--- funkcje/zadanie5.py
+++ funkcje/zadanie5.py
@@ -11,17 +11,22 @@ Napisać 6 funkcji przeliczających temperatury:
 # def przelicz_C_na_K(t):
 # itd
 
+def funkcja_liniowa(a, b):
+    def f(x):
+        return a*x+b
+    return f
+
 def przelicz_C_na_K(t):
-    return t + 273.15
+    return funkcja_liniowa(1, 273.15)(t)
 
 def przelicz_K_na_C(t):
-    return t - 273.15
+    return funkcja_liniowa(1, -273.15)(t)
 
 def przelicz_C_na_F(t):
-    return 1.8 * t + 32
+    return funkcja_liniowa(1.8, 32)(t)
 
 def przelicz_F_na_C(t):
-    return (t - 32)/1.8
+    return funkcja_liniowa(5/9,-17.78)(t)
 
 def przelicz_K_na_F(t):
     return przelicz_C_na_F(przelicz_K_na_C(t))
@@ -29,8 +34,12 @@ def przelicz_K_na_F(t):
 def przelicz_F_na_K(t):
     return przelicz_C_na_K(przelicz_F_na_C(t))
 
-
 temperatury = [ -50, -20, -10, -1, 0, 1, 5, 15, 25, 70, 100, 200, 300 ]
 
 for temperatura in temperatury:
     print(f"{temperatura:10.2f}C = {przelicz_C_na_F(temperatura):10.2f}F = {przelicz_C_na_K(temperatura):10.2f}K")
+
+temperaturyF = [ -10, 0, 10, 32, 60, 212 ]
+
+for temperatura in temperaturyF:
+    print(f"{temperatura:10.2f}F = {przelicz_F_na_C(temperatura):10.2f}C")
