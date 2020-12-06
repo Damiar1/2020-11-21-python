9cfb5f7d20af08ea33bd3a17fbf538967401f546
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 12:03:21 +0100

    zadanie - rozwiązanie

--- funkcje/zadanie3.py
+++ funkcje/zadanie3.py
@@ -6,20 +6,27 @@ Przy co najmniej 150 osobach koszt jest stały i wynosi 1600.00 za całość.
 """
 
 def koszt_biletow_grupy(liczba_osob, cennik):
-    return liczba_osob * ( cennik["cena_grupowa"] if liczba_osob >= cennik["min_grupa"] else cennik["cena_zwykla"] )
+    return ( cennik['cena_sali'] if liczba_osob >= cennik['min_sala']
+        else liczba_osob * ( cennik["cena_grupowa"] if liczba_osob >= cennik["min_grupa"]
+        else cennik["cena_zwykla"] ) )
 
 grupy_testowe = [ 1, 2, 5, 7, 9, 13, 20, 50, 140, 160 ]
 
 c1 = {
     "cena_zwykla" : 20,
     "cena_grupowa" : 13,
-    "min_grupa" : 10
+    "min_grupa" : 10,
+    "cena_sali" : 1600,
+    "min_sala" : 150
 }
 
 c2 = {
     "cena_zwykla": 19,
     "cena_grupowa": 14,
-    "min_grupa": 12
+    "min_grupa": 12,
+    "cena_sali": 1600,
+    "min_sala": 150
+
 }
 
 for g in grupy_testowe:
