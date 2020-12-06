07ce4edbf70e67ee5b34ecabde5acdb6b0b6fbab
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 14:57:40 +0100

    zadanie Fibonacci

---
+++ funkcje/zadanie6.py
@@ -0,0 +1,12 @@
+# zaimplementowac funkcję fib(n)
+# obliczającą n-tą wartość w ciągu Fibonacciego
+# metodą rekurencyjną
+#
+# np. fib(0) -> 0
+# fib(1) -> 1
+# fib(2) -> 1
+# fib(3) -> 2
+# fib(4) -> 3
+# fib(5) -> 5
+# fib(6) -> 8 itd
+
