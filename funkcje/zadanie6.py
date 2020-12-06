fe7da14b910191d22868015123cd3ea7d5e7a348
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-06 15:09:48 +0100

    zadanie Fibonacci - implementacja

--- funkcje/zadanie6.py
+++ funkcje/zadanie6.py
@@ -10,3 +10,13 @@
 # fib(5) -> 5
 # fib(6) -> 8 itd
 
+def fib(n):
+    print(f"Obliczam {n}. element ciągu Fibonacciego")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+print(fib(7))
+
