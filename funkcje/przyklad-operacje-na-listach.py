e87dc91cf71a9e3cd3f555b75bd34a7842c4bc36
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-19 10:43:16 +0100

    filter()

--- funkcje/przyklad-operacje-na-listach.py
+++ funkcje/przyklad-operacje-na-listach.py
@@ -4,10 +4,17 @@ liczby = [3, 5, 10, 11, 15, 1, 23, 2, 3, 10, 0, 4, 17, 7]
 #    return 3*x
 
 #liczby3 = list(map(trzykrotnosc, liczby))
-
 liczby3 = list(map(lambda x:3*x, liczby))
-
 liczby3bis = [ 3*x for x in liczby ]
-
 print(liczby3)
 print(liczby3bis)
+
+# tylko większe równe od 10
+def ge10(x):
+    return x >= 10
+
+liczby10 = list(filter(ge10,liczby))
+liczby10bis = [ x for x in liczby if x>=10 ]
+print(liczby10)
+print(liczby10bis)
+
