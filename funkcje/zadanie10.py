8a2e60f2bcdc012168b876e071106cd4848abfec
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-19 10:02:21 +0100

    rozwiązanie BMI

--- funkcje/zadanie10.py
+++ funkcje/zadanie10.py
@@ -1,7 +1,7 @@
 osoba1 = {"imie":"Xawery", "wzrost":1.81, "waga":72 }
 osoba2 = {"imie":"Zdzisław", "wzrost":1.61, "waga":92 }
 osoba3 = {"imie":"Zygmunt", "wzrost":1.91, "waga":71 }
-osoba4 = {"imie":"Alojzy", "wzrost":1.72, "waga":82 }
+osoba4 = {"imie":"Alojzy", "wzrost":1.72, "waga":93 }
 
 osoby = [ osoba1, osoba2, osoba3, osoba4 ]
 
@@ -13,6 +13,8 @@ osoby.sort(key=lambda osoba:-osoba["wzrost"])
 osoby.sort(key=lambda osoba:osoba["wzrost"], reverse=True)
 
 # wypisz osoby posortowane wg BMI
+
+osoby.sort(key=lambda o:o['waga']/(o['wzrost']**2))
 
 for o in osoby:
     print(o)
