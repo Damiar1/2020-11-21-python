18a6dac59708f377bbadf694e91a941cce0750aa
Mateusz Adamowski <m.adamowski@alx.pl>
2020-12-19 09:41:33 +0100

    zadanie 10

---
+++ funkcje/zadanie10.py
@@ -0,0 +1,8 @@
+osoba1 = {"imie":"Xawery", "wzrost":1.81, "waga":72 }
+osoba2 = {"imie":"Zdzisław", "wzrost":1.61, "waga":92 }
+osoba3 = {"imie":"Zygmunt", "wzrost":1.91, "waga":71 }
+osoba4 = {"imie":"Alojzy", "wzrost":1.72, "waga":82 }
+
+osoby = [ osoba1, osoba2, osoba3, osoba4 ]
+
+# wypisz osoby posortowane wg wzrostu
