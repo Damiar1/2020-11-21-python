a01e013bc98b16f2beb33c9875fc005ac03fc359
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-09 12:52:21 +0100

    JSON - zadanie

---
+++ pliki/zadanie03.py
@@ -0,0 +1,10 @@
+# wczytać uczniów z pliku `uczniowie.json`
+# policzyć średnią
+# do pliku `najlepsi.json` wpisać 3 uczniów z najwyższą średnią w formacie:
+# [
+#   {
+#    "kto" : "Jan K.",
+#    "srednia" : 4.45
+#   },
+# ...
+# ]
