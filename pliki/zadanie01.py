6ad685a1f1d57bb0dadf3d560472324f63a06dc3
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-09 11:30:01 +0100

    rozwiązanie zadania tankowanie

--- pliki/zadanie01.py
+++ pliki/zadanie01.py
@@ -1,3 +1,14 @@
 # wczytać dane z pliku tankowania.txt
 # dla każdego tankowania podać koszt paliwa
 # podać sumaryczny koszt paliwa
+
+kosztCalk = 0.0
+for linia in open("tankowania.txt"):
+    kolumny = linia.strip().split(',')
+    obj = float(kolumny[2])
+    cena = float(kolumny[3])
+    koszt = obj * cena
+    kosztCalk += koszt
+    print(f"Tankowanie {obj}L * {cena} PLN = {koszt:.2f} PLN")
+
+print(f"Razem: {kosztCalk:.2f} PLN")
