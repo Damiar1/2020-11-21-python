aee9016799baabc218532db195e65969d1b4be3b
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-09 14:44:35 +0100

    zadanie - waluty

---
+++ internet/zadanie01.py
@@ -0,0 +1,7 @@
+# napisać program do przeliczania kursów walut
+
+# 1. użytkownik podaje kwotę i walutę
+
+# 2. użytkownik podaje drugą walutę
+
+# 3. program przelicza kwotę z jednej waluty na drugą (wg ostatniego obowiązującego kursu NBP)
