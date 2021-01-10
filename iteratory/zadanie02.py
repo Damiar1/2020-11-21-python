cee58bc77a707df3776d74058e67ad07f124d6cf
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-10 13:43:09 +0100

    zadanie 2 - rozwiązanie

--- iteratory/zadanie02.py
+++ iteratory/zadanie02.py
@@ -1,5 +1,11 @@
 # Napisać generator, który z danego słowa zwróci wszystkie samogłoski
 
+def samogloski(napis):
+    SAMOGLOSKI = 'aeuioyAEUIOY'
+    for litera in napis:
+        if litera in SAMOGLOSKI:
+            yield litera
+
 # Przykład:
 
 napis = "Ala ma kota"
