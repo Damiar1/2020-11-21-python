8e41d839f3b0cec1bc91016377dd10cf3590fab1
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-10 14:32:27 +0100

    pierwsze rozwiązanie

--- iteratory/zadanie03.py
+++ iteratory/zadanie03.py
@@ -1,5 +1,11 @@
 # dopisać generator kazdy_z_kazdym()
 
+def kazdy_z_kazdym(zawodnicy):
+    for i in range(len(zawodnicy)):
+        grupa = zawodnicy[i+1:]
+        zawodnik1 = zawodnicy[i]
+        for zawodnik2 in grupa:
+            yield zawodnik1, zawodnik2
 
 zawodnicy = ["Mateusz", "Maciej", "Piotr", "Dariusz", "Andrzej"]
 
