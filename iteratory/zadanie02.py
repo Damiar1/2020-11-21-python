c4748257b6925a807b1d8f120eeea9887fddc936
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-10 12:46:40 +0100

    zadanie 2

---
+++ iteratory/zadanie02.py
@@ -0,0 +1,15 @@
+# Napisać generator, który z danego słowa zwróci wszystkie samogłoski
+
+# Przykład:
+
+napis = "Ala ma kota"
+
+for s in samogloski(napis):
+    print("* s")
+
+# ma wyświetlić:
+# * A
+# * a
+# * a
+# * o
+# * a
