a0f8a312da0f95c81f9658e2b957365663b74598
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-10 15:05:18 +0100

    zadanie 4 + przyklad z yield

--- iteratory/zadanie04.py
+++ iteratory/zadanie04.py
@@ -1,5 +1,7 @@
 # Mając listę wartości, policzyć wszystkie możliwe permutacje tej listy.
 
+
+
 # np. A B C:
 #
 # A B C
@@ -9,7 +11,22 @@
 # C A B
 # C B A
 
-osoby = ('Andrzej', 'Basia', 'Czesław')
+def permutacje(elementy):
+    #print(f"Permutacje: {elementy}")
+    if len(elementy) == 1:
+        yield elementy
+    # elif len(elementy) == 2:
+    #     yield elementy
+    #     yield elementy[::-1]
+    else:
+        for n, e in enumerate(elementy):
+            pozostale = elementy[0:n] + elementy[n+1:]
+            biezacy = elementy[n:n+1]
+            for p in permutacje(pozostale):
+                yield biezacy + p
 
-for x in permutacje(osoby):
-    print(x)
+osoby = ('Andrzej', 'Basia', 'Czesław', 'Dariusz', 'Edward', 'Franciszek', 'Grzegorz')
+
+for n, x in enumerate(permutacje(osoby)):
+    print(n+1, x)
+print("KONIEC")
