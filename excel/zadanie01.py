d0f8bbf151de7340e1bf00a3e7742f04d19ec10c
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-10 16:00:33 +0100

    link do openpyxl

--- excel/zadanie01.py
+++ excel/zadanie01.py
@@ -1,5 +1,7 @@
 # instalacja modułu openpyxl:
 # py -m pip install openpyxl
+# dokumentacja:
+# https://openpyxl.readthedocs.io/en/stable/usage.html
 
 # wczytać informacje o tankowaniach z pliku json
 # i wygenerować raport w formacie Excela
