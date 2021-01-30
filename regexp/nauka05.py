8c6ce97964c83db81e1c819031df75db7546b4e3
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-30 10:05:41 +0100

    weryfikowanie kodów pocztowych - ciąg dalszy

--- regexp/nauka05.py
+++ regexp/nauka05.py
@@ -1,14 +1,10 @@
 import re
 
-kody = ['01-456', '23-b34', '00-000', '12345', '34-554', 'brak', 'xx-xxx']
+kody = ['01-456', '23-b34', '00-000', '12345', '34-554', 'brak', 'xx-xxx', 'abc00-123x', ' 00-123', '00-456 Warszawa']
 
-wzorzec = re.compile('[0-9]{2}-[0-9]{3}')
+wzorzec = re.compile('[0-9]{2}-[0-9]{3}$')
 
 for kod in kody:
     x = wzorzec.match(kod)
-    print(kod, x)
-    if x:
-        print("OK")
-    else:
-        print("Nie ok")
+    print(kod, "OK" if x else "BLAD", x.group() if x is not None else "---")
 
