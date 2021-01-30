35f98902c206b721b6f9042c0257d60c7ec7fdec
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-30 15:48:38 +0100

    pakiet nauka, moduł geometria

--- program/nauka3.py
+++ program/nauka3.py
@@ -1,5 +1,13 @@
-import geometria
+import nauka.geometria
+from nauka import geometria
+from nauka.geometria import Dlugosc
 
-d1 = geometria.Dlugosc(6, 'km')
-d2 = geometria.Dlugosc(4, 'mi')
+d1 = nauka.geometria.Dlugosc(6, 'km')
+d2 = nauka.geometria.Dlugosc(4, 'mi')
 print(d1 + d2)
+
+d3 = geometria.Dlugosc(1, 'km')
+d4 = geometria.Dlugosc(1, 'mi')
+print(d3 + d4)
+
+print(Dlugosc(5,'m') + Dlugosc(14,'ft'))
