288439964215ccfc8176be7cc953cc7c563e1672
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-31 14:02:47 +0100

    Przekazywanie wartości do szablonu, pętle w szablonach.

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from datetime import datetime
 #from django.http import HttpResponse
+
+from .models import Article
 
 # Create your views here.
 
 def homepage(request):
-    return render(request, "articles/homepage.html")
+    return render(request, "articles/homepage.html", {
+        "d": datetime.now(),
+        "articles": Article.objects.filter()
+    })
 
 #def homepage(request):
 #    return HttpResponse("hurra! dziala")
