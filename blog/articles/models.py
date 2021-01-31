9a4c2e46075de24e123242400c7fa1df6d5f9089
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-31 12:23:25 +0100

    implementacja modeli

--- blog/articles/models.py
+++ blog/articles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField()
+    description = models.TextField()
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
