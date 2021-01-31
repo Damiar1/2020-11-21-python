86b508438564bb1b38136e992d048e3e9119bac5
Mateusz Adamowski <m.adamowski@alx.pl>
2021-01-31 12:49:16 +0100

    integracja z panelem admina c.d.

--- blog/articles/admin.py
+++ blog/articles/admin.py
@@ -5,7 +5,19 @@ from django.contrib import admin
 from .models import Article
 from .models import Category
 
-admin.site.register(Category)
-admin.site.register(Article)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'slug'
+    )
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug'
+    )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
 
 
